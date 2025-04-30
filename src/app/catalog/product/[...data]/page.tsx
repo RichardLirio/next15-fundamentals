2d0ac0c9e26bbb2ec8b69a311e1b12d4c00b5e7f
Richard Lirio <richardlirio@hotmail.com>
2025-04-30 18:34:43 -0300

    feat: react server components and client components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,17 +1,32 @@
+"use client";
+
+import * as React from "react";
+import { use } from "react";
+
 interface ProductProps {
-  params: {
-    data: string[];
-  };
+  data: string[];
 }
 
-function Product({ params }: ProductProps) {
-  const [productId, size, color] = params.data;
+// Server components => A gente não USA JavaScript no lado do cliente(componentes com visuais estaticos)
+// Client components => O JavaScript é enviado ao navegador(cliente)
+
+function Product({ params }: { params: Promise<ProductProps> }) {
+  const { data } = use(params);
+  const [productId, size, color] = data;
+
+  function addToCart() {
+    console.log("Adicionou ao carrinho");
+  }
 
   return (
     <div>
       <p>Product id: {productId}</p>
       <p>Size: {size}</p>
       <p>color {color}</p>
+
+      <button onClick={addToCart} className="cursor-pointer">
+        Adicionar ao carrinho{" "}
+      </button>
     </div>
   );
 }
